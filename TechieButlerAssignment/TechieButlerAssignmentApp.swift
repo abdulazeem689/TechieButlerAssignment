8dbc4958551e29b3ee75864aafb95d9354964a01
Abdul Azeem <abdul.tech689@gmail.com>
2024-05-01 18:00:27 +0530

    Initial Commit

---
+++ TechieButlerAssignment/TechieButlerAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechieButlerAssignmentApp.swift
+//  TechieButlerAssignment
+//
+//  Created by Abdul Azeem on 01/05/24.
+//
+
+import SwiftUI
+
+@main
+struct TechieButlerAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
